4812d8c552c06269bffbf11e6d13de9001657785
abumeis <37344104+abumeis@users.noreply.github.com>
2021-01-11 16:26:21 +0100

    exo18

---
+++ exercice18/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#button").click(function() {
+        $("#text").hide();
+    })
+});
+
+//on peut le faire avec css méthode aussi: $("#text").css("display", "none");
+//
