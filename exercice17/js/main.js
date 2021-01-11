c85d75316b67363945088b717a4eacec921a0079
abumeis <37344104+abumeis@users.noreply.github.com>
2021-01-11 16:23:36 +0100

    exo17

---
+++ exercice17/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#button").click(function() {
+        $("#text").show();
+    })
+});
+
+//on peut le faire avec css méthode aussi: $("#text").css("display", "block");
+//
