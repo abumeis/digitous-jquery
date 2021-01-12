b07d265ee2f38caf27ae7c41ff06a378119bc4c6
abumeis <37344104+abumeis@users.noreply.github.com>
2021-01-12 14:06:43 +0100

    exo34

--- exercice34/js/main.js
+++ exercice34/js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
     $("button").click(function() {
         $.ajax({
-            url: 'https://restcountries.eu/rest/v2/name/', /// normalment c'est le nom de l'input qui compte 
-            //mais ce synthax $("input").val(), me donne une errore 
+            url: `https://restcountries.eu/rest/v2/name/${$("input").val()}`, // c'était à cause de ça {}, mail là ça march yoooho
             success: function(data) {
                 $("#country").html(data[0].name);
-                $("#capital").html(data[0].capital);
+                $("#capital").html(data[0].capital); //D'ailleurs comment ça s'écrit le syntaxe HTML ici ??
             }
         });
     })
